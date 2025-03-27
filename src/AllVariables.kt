16c7992a9b84eb7b1f06178050056b072433103e
Burakcan <brkcnszgn@gmail.com>
2025-03-27 14:51:27 +0300

    all variables

---
+++ src/AllVariables.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val smallNumber : UByte = 255u
+    val mediumNumber : UShort = 65535u
+    val largeNumber  = 9_000_000_000uL
+
+    val pi = 3.14
+    val pi2 = 3.14f
+
+    val isKotlinFun = true
+    val isJavaFun = false
+
+    val firstLetterString = "K" //String
+    val firstLetterChar = '5' //String
+
+    val convert = firstLetterChar.toFloat()
+}
